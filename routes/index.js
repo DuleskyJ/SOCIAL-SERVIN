56fe6c324d0e94e1ab12cbe804911a2c4d4f1f96
Heroges <77005710+Heroges@users.noreply.github.com>
2024-07-02 21:50:05 +0700

    Delete routes directory

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const thoughtRoutes = require('./thoughtRoutes');
-
-router.use('/users', userRoutes);
-router.use('/thoughts', thoughtRoutes);
-
-module.exports = router;
